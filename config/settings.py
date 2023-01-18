e782d4b6a08bb7a4c839fe4f7e2e1302b8b9d9f2
ryuji <ryujinho63@gmail.com>
2023-01-19 00:50:23 +0900

    test

--- config/settings.py
+++ config/settings.py
@@ -182,7 +182,7 @@ if DEBUG:
     CSRF_TRUSTED_ORIGINS = ["http://127.0.0.1:3000"]
 else:
     CORS_ALLOWED_ORIGINS = ["https://mypo.ink"]
-    CSRF_TRUSTED_ORIGINS = ["https://mypo.ink/", "https://mypo.ink", "http://mypo.ink/", "http://mypo.ink", "https://www.mypo.ink/", "https://www.mypo.ink", "www.mypo.ink"]
+    CSRF_TRUSTED_ORIGINS = ["https://mypo.ink"]
 
 CORS_ALLOW_CREDENTIALS = True
 
